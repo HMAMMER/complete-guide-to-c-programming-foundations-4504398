b7f30d4fbba7b50f79f51e0828cbe6ac2f401b8c
HMAMMER <megofish123@gmail.com>
2025-12-19 11:28:21 -0500

    still trying to figure out

--- CH07/07_12/07_12-challenge1.c
+++ CH07/07_12/07_12-challenge1.c
@@ -9,27 +9,33 @@
 int main()
 {
 	char userInput[10];
-	char upperOutput[10];
-	int i;
+	char userOutput[10];
+	char quit = 'QUIT';
+	char ch;
+	int i = 0;
 
 	while(1)
 	{
 		printf("Command: ");
 		
 		fgets(userInput, 10, stdin);
-		// fetch input
+	
+		while (userInput[i]) {
+			ch = userInput[i];
+			strcat(userOutput,(toupper(ch)));
+			i++;
+		}
+
 		for(i = 0; i < 10; i++){
 			if (userInput[i] == '\n'){
 				userInput[i] = '\0';
 			}
-		}
-		// convert to uppercase
-		// output "Processing command 'cmd'"
-		printf("%s", userInput);
-		// break the loop on `QUIT`
-		if (userInput == "QUIT"){
-			break;
 		} 
+
+		printf("Processing command %s", userOutput);
+
+		if( strcmp(userOutput,quit)==0 )
+			break;
 	}
 
 	return 0;
